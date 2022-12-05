d0e778972746842f9a5dc59efb49fe99bcec4223
Matias Barril <matibarril@gmail.com>
2022-12-05 13:35:04 -0300

    first steps

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    printwidth: 200, //cuantos caracteres son impresos en pantalla
+    tabWidth: 2,
+    useTabs: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+}
